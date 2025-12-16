2cfc8c4925bbafb9ec9267b69ba4ef7758f275d9
E. K. Lee <wolf@teoal.net>
2025-12-16 20:23:11 +0900

    1-3-5 실습 완료

---
+++ ch01/recipe_1_3_5/src/main/java/com/apress/spring6recipes/sequence/Main.java
@@ -0,0 +1,17 @@
+package com.apress.spring6recipes.sequence;
+
+import com.apress.spring6recipes.sequence.config.SequenceConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(SequenceConfiguration.class)) {
+            String[] beans = context.getBeanDefinitionNames();
+            var generator = context.getBean("sequence", Sequence.class);
+
+            System.out.println(generator.getSequence());
+            System.out.println(generator.getSequence());
+        }
+    }
+}
---
+++ ch01/recipe_1_3_5/src/main/java/com/apress/spring6recipes/sequence/NumberPrefixGenerator.java
@@ -0,0 +1,13 @@
+package com.apress.spring6recipes.sequence;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class NumberPrefixGenerator implements PrefixGenerator {
+
+    @Override
+    public String getPrefix() {
+        var randomGenerator = ThreadLocalRandom.current();
+        return String.format("%03d", randomGenerator.nextInt(100));
+    }
+}
