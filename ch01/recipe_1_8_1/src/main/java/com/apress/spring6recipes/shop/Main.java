cabf5f408556b29101607f93588d285d1bea8a64
E. K. Lee <wolf@teoal.net>
2025-12-17 15:20:37 +0900

    1-8-1 실습 완료

---
+++ ch01/recipe_1_8_1/src/main/java/com/apress/spring6recipes/shop/Main.java
@@ -0,0 +1,27 @@
+package com.apress.spring6recipes.shop;
+
+import com.apress.spring6recipes.shop.config.ShopConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        try (var context = new AnnotationConfigApplicationContext(ShopConfiguration.class)) {
+            var aaa = context.getBean("aaa", Product.class);
+            var cdrw =  context.getBean("cdrw", Product.class);
+            var dvdrw =  context.getBean("dvdrw", Product.class);
+
+            var cart1 = context.getBean("shoppingCart", ShoppingCart.class);
+            cart1.addItem(aaa);
+            cart1.addItem(cdrw);
+            System.out.println("장바구니 1에는: " + cart1.getItems());
+            var cart2 = context.getBean("shoppingCart", ShoppingCart.class);
+            cart2.addItem(dvdrw);
+            System.out.println("장바구니 2에는: " + cart2.getItems());
+
+            var cashier = context.getBean("cashier", Cashier.class);
+            cashier.checkout(cart1);
+            cashier.checkout(cart2);
+        }
+    }
+}
---
+++ ch01/recipe_1_8_1/src/main/java/com/apress/spring6recipes/shop/config/ShopConfiguration.java
@@ -0,0 +1,37 @@
+package com.apress.spring6recipes.shop.config;
+
+import com.apress.spring6recipes.shop.Battery;
+import com.apress.spring6recipes.shop.Cashier;
+import com.apress.spring6recipes.shop.Disc;
+import com.apress.spring6recipes.shop.Product;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.apress.spring6recipes.shop")
+public class ShopConfiguration {
+
+    @Bean
+    public Product aaa() {
+        return new Battery("AAA", 2.5, true);
+    }
+
+    @Bean
+    public Product cdrw() {
+        return new Disc("CD-RW", 1.5, 700);
+    }
+
+    @Bean
+    public Product dvdrw() {
+        return new Disc("DVD-RW", 3.0, 4700);
+    }
+
+    // 이걸로 Cashier 빈을 만들면서 openFile 자동 실행, 끝날 때 closeFile 자동 실행
+    @Bean(initMethod = "openFile", destroyMethod = "closeFile")
+    public Cashier cashier() {
+        System.out.println(System.getProperty("java.io.tmpdir"));
+        var path = System.getProperty("java.io.tmpdir") + "/casher";
+        return new Cashier("checkout", path);
+    }
+}
