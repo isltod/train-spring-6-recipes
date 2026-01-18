5a1f14377180fa32719aa01b869e23bfd9f18da7
E. K. Lee <wolf@teoal.net>
2026-01-18 21:44:33 +0900

    1-2까지 재검토

--- ch01/recipe_1_1_2/src/main/java/com/apress/spring6recipes/sequence/Main.java
+++ ch01/recipe_1_1_2/src/main/java/com/apress/spring6recipes/sequence/Main.java
@@ -10,7 +10,7 @@ public class Main {
         // @ComponentScan을 사용하면 @Configuration 클래스를 지정하면 예외 발생...
         // var cfg = SequenceConfiguration.class;
         // try (var context = new AnnotationConfigApplicationContext(cfg)) {
-        
+
         // 아래처럼 패키지 이름을 지정해야 한다.
         var basePackages = "com.apress.spring6recipes.sequence";
         try (var context = new AnnotationConfigApplicationContext(basePackages)) {
