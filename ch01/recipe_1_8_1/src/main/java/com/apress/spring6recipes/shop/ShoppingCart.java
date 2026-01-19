51bd14175270133bf44b4820aa20b894588c9359
E. K. Lee <wolf@teoal.net>
2026-01-19 17:08:28 +0900

    1-8까지 재검토

--- ch01/recipe_1_8_1/src/main/java/com/apress/spring6recipes/shop/ShoppingCart.java
+++ ch01/recipe_1_8_1/src/main/java/com/apress/spring6recipes/shop/ShoppingCart.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope("prototype")
 // 이걸 지정하면 다른데서 불러질 때까지 기다렸다가 초기화 한다고...근데 지금은 이점이 뭔지 잘 모르겠네...
+// @Autowired와 함께 사용하면 DI를 지연시킬 수도 있다...
 @Lazy
 public class ShoppingCart {
 
