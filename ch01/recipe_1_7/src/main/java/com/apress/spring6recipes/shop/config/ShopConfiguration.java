a9b371d45e632a4cf36ab1199a53b4c7b3439035
E. K. Lee <wolf@teoal.net>
2026-01-19 16:47:10 +0900

    1-7까지 재검토

--- ch01/recipe_1_7/src/main/java/com/apress/spring6recipes/shop/config/ShopConfiguration.java
+++ ch01/recipe_1_7/src/main/java/com/apress/spring6recipes/shop/config/ShopConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
+import com.apress.spring6recipes.shop.Cachier;
+import com.apress.spring6recipes.shop.ShoppingCart;
+
 @Configuration
 public class ShopConfiguration {
 
@@ -13,7 +16,18 @@ public class ShopConfiguration {
         // resources 아래에서 messages로 시작하는 파일이라고...messages_언어코드_국가코드.properties
         // ko KR은 getMessage에서 Locale.KOREA로 지정하면 자동으로 그 파일을 찾는다...
         messageSource.setBasename("classpath:messages");
+        // 실제로 파일이 변하지 않으면 캐싱을 하지 않으므로 적당히 짧게 주는게 좋다...
         messageSource.setCacheSeconds(1);
         return messageSource;
+    }
+
+    @Bean
+    public Cachier cachier() {
+        return new Cachier();
+    }
+
+    @Bean
+    public ShoppingCart shoppingCart() {
+        return new ShoppingCart();
     }
 }
