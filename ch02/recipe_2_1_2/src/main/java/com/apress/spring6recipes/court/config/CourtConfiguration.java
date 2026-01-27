0a75c9639c026379e5d4986a33c2238783d4e643
E. K. Lee <wolf@teoal.net>
2026-01-27 22:55:08 +0900

    2-1 스프링 MVC 구성 실습 완료

---
+++ ch02/recipe_2_1_2/src/main/java/com/apress/spring6recipes/court/config/CourtConfiguration.java
@@ -0,0 +1,24 @@
+package com.apress.spring6recipes.court.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("com.apress.spring6recipes.court")
+// DispatcherServlet에도 뭔가 설정했는데,
+// 여기서 또 추가 설정하고, WebMvcConfigurer 인스턴스를 이용해 변경할 수 있게 해준다...
+// 왜 스프링부트를 쓰지 않나? 왜 이렇게 복잡하게 해야 하나...
+@EnableWebMvc
+public class CourtConfiguration {
+
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+        var viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
